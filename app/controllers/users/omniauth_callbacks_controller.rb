fd5e9d07577f19bf934208c89efdd1fc5bd47df0
Diego Silva <diegospp20@gmail.com>
2026-08-20 03:38:39 -0300

    feat: improve Google OAuth name parsing, error handling, and update authentication UI with placeholder buttons

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,17 +1,26 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
-    @user = User.from_omniauth(request.env['omniauth.auth'])
+    auth = request.env['omniauth.auth']
+    if auth.blank?
+      flash[:alert] = "Nenhuma informação de autenticação foi recebida do Google."
+      redirect_to new_user_session_path and return
+    end
+
+    @user = User.from_omniauth(auth)
 
     if @user.persisted?
       flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
       sign_in_and_redirect @user, event: :authentication
     else
-      session['devise.google_data'] = request.env['omniauth.auth'].except('extra') # Removing extra as it can overflow some session stores
-      redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
+      session['devise.google_data'] = auth.except('extra')
+      redirect_to new_user_registration_url, alert: @user.errors.full_messages.to_sentence
     end
   end
 
   def failure
-    redirect_to root_path
+    error_msg = params[:message].presence || "Autenticação cancelada ou não autorizada"
+    Rails.logger.error("[OmniAuth Failure] Mensagem: #{error_msg}")
+    flash[:alert] = "Não foi possível concluir o login com o Google (#{error_msg.humanize}). Tente novamente ou use e-mail e senha."
+    redirect_to new_user_session_path
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -49,19 +49,19 @@ class User < ApplicationRecord
           u.uid = auth.uid
           u.email = auth.info.email
           u.password = Devise.friendly_token[0, 20]
-          u.first_name = auth.info.first_name
-          u.last_name = auth.info.last_name
+          u.first_name = auth.info.first_name.presence || auth.info.name&.split(' ')&.first || "Usuário"
+          u.last_name = auth.info.last_name.presence || auth.info.name&.split(' ')&.drop(1)&.join(' ')
         end
       end
     end
     
-    # 5. Se o Google enviou uma foto de perfil e o usuário não tem uma, tentamos anexar
+    # 5. Se o Google enviou uma foto de perfil e o usuário não tem uma, anexamos de forma segura
     if auth.info.image.present? && !user.avatar.attached?
       begin
         downloaded_image = URI.open(auth.info.image)
         user.avatar.attach(io: downloaded_image, filename: "google_avatar_#{user.id}.jpg")
       rescue StandardError => e
-        Rails.logger.error "Ocorreu um erro ao baixar a foto do Google: #{e.message}"
+        Rails.logger.error "[OmniAuth] Erro ao baixar avatar do Google: #{e.message}"
       end
     end
     
